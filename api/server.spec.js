e5aa8f0584f6a856015c119f057a4004968632b3
Christene Bennett <christene@Christenes-MacBook-Pro.local>
2019-05-01 08:44:51 -0700

    Add tests for server

---
+++ api/server.spec.js
@@ -0,0 +1,22 @@
+const server = require('./server.js');
+const request = require('supertest');
+const db = require('../data/dbConfig');
+
+describe('the server', () => {
+
+  describe('GET /', () => {
+    it('should return status 200', async () => {
+      const res = await request(server).get('/');
+      expect(res.status).toBe(200);
+    })
+    it('should return { api: "up and running" }', async () => {
+      const res = await request(server).get('/');
+      expect(res.body).toEqual({ 'api': 'up and running' })
+    })
+    it('should return JSON', async () => {
+      const res = await request(server).get('/');
+      expect(res.type).toBe('application/json')
+    })
+  })
+
+})
