b18a5397508a121a1429c9f9550cb2f6b3efda71
Christene Bennett <christene@Christenes-MacBook-Pro.local>
2019-05-01 21:22:02 -0700

    Add authRouter tests

--- auth/authRouter.spec.js
+++ auth/authRouter.spec.js
@@ -6,32 +6,36 @@ const testUser = {
   "username": "bobtest",
   "password": "pass123"
 }
+
 describe('the auth router', () => {
 
-  describe('POST /', () => {
+  describe('insert new user', () => {
     beforeEach(() => {
       return db('parents').truncate();
     })
-    it('should return status 201', async () => {
-      
-      const res = await request(server)
-        .post('/register')
-        .send(testUser);
-      expect(res.status).toBe(201);
+
+    it('should insert a user into the db', async () => {
+      const [id] = await db('parents').insert(testUser);
+      expect(id).toBe(1);
     })
 
-    xit('should return status 400 when body is not correct', async () => {
-      const body = {}
-      const res = await request(server)
-        .post('/register')
-        .send(body)
-      expect(res.status).toBe(400);
+  })
+
+  describe('retrieve existing user', () => {
+    const testUser2 = {
+      "username": "bobtest",
+      "password": "pass123"
+    }
+
+    beforeEach(() => {
+      return db('parents').truncate();
     })
 
-    xit('should return JSON', async () => {
-      const res = await request(server).get('/');
-      console.log(res)
-      expect(res.type).toBe('application/json')
+    it('should return existing user from db', async () => {
+      const [id] = await db('parents').insert(testUser);
+      const parent = await db('parents').where({id});
+      console.log(parent)
+      expect(id).toBe(1)
     })
   })
 
