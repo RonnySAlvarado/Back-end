9bfba0f92883b2497f3372ef1495021225f3acf9
Christene Bennett <christene@Christenes-MacBook-Pro.local>
2019-05-03 20:17:35 -0700

    Add integration tests

--- auth/authRouter.spec.js
+++ auth/authRouter.spec.js
@@ -8,6 +8,12 @@ const testUser = {
   "password": "pass123"
 }
 
+const testChild = {
+  "name": "Jacob",
+  "parentId": 1,
+  "gigapetName": "Dino Doug"
+}
+
 describe('the auth router', () => {
 
   describe('insert new user', () => {
@@ -38,6 +44,94 @@ describe('the auth router', () => {
       expect(id).toBe(1)
       expect(parent.username).toBe('bobtest')
     })
+  })
+
+  describe('POST /register and /login', () => {
+
+    beforeEach(() => {
+      return db('parents').truncate();
+    })
+
+    it('should return status 200 after registering', async () => {
+      const res = await request(server)
+        .post('/api/register')
+        .send(testUser);
+      expect(res.status).toBe(201);
+    })
+
+    it('should return status 200 after logging in', async () => {
+      await request(server)
+        .post('/api/register')
+        .send(testUser);
+      const res = await request(server)
+        .post('/api/login')
+        .send(testUser);
+      const { id } = res.body;
+      expect(res.status).toBe(200);
+      expect(id).toBe(1);
+    })
+
+    it('should return JSON', async () => {
+      const res = await request(server).get('/');
+      expect(res.type).toBe('application/json')
+    })
+  })
+  
+  describe('Adding children', () => {
+
+    beforeEach(() => {
+      return db('parents').truncate();
+      return db('children').truncate();
+    })
+
+    it('should return status 200 for children endpoint', async () => {
+      const parent = await request(server)
+      .post('/api/register')
+      .send(testUser);
+      
+      const { id, token } = parent.body;
+
+      const children = await request(server)
+      .get(`/api/parents/${id}/children`)
+      .set('Authorization', token);
+        expect(children.status).toBe(200);
+    })
+
+    it('should return status 201 for adding child', async () => {
+      const parent = await request(server)
+      .post('/api/register')
+      .send(testUser);
+      
+      const { id, token } = parent.body;
+
+      const children = await request(server)
+      .post(`/api/parents/${id}/children`)
+      .send(testChild)
+      .set('Authorization', token);
+      
+      const {newChild} = children.body;
+      
+      expect(children.status).toBe(201);
+      expect(newChild.name).toBe('Jacob');
+      expect(newChild.parentId).toBe(1);
+    })
+
+    it ('should return 500 when req body is not complete', async () => {
+      const parent = await request(server)
+      .post('/api/register')
+      .send(testUser);
+      
+      const { id, token } = parent.body;
+
+      const children = await request(server)
+      .post(`/api/parents/${id}/children`)
+      .send({})
+      .set('Authorization', token);
+          
+      expect(children.status).toBe(500);
+      
+    })
+
   })
 
 })
--- children/children.spec.js
+++ children/children.spec.js
@@ -1,20 +1,4 @@
-const server = require('../api/server');
-const request = require('supertest');
 const db = require('../data/dbConfig');
-
-describe('the children router', () => {
-
-  describe('GET /', () => {
-    xit('should return status 200', async () => {
-      const res = await request(server).get('/api/children');
-      expect(res.status).toBe(200);
-    })
-    
-    it('should return JSON', async () => {
-      const res = await request(server).get('/');
-      expect(res.type).toBe('application/json')
-    })
-  })
 
   describe('children db helpers', () => {
     const testChild = {
@@ -36,5 +20,3 @@ describe('the children router', () => {
   
     })
   })
-
-})
