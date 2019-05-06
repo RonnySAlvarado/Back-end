bc2c7c7596e682eb4ffd6be7a2d2dc8f431c7ba7
Christene Bennett <christene@Christenes-MBP.strongmind.com>
2019-05-06 12:31:01 -0700

    Add foods to foods seed data

--- data/seeds/04-foods.js
+++ data/seeds/04-foods.js
@@ -12,6 +12,28 @@ exports.seed = function(knex, Promise) {
         {id: 5, name: 'cheese', value: -4, 'categoryId': 5},
         {id: 6, name: 'avocado', value: 4, 'categoryId': 6},
         {id: 7, name: 'cake', value: -4, 'categoryId': 7},
+        {id: 8, name: 'banana', value: 1, 'categoryId': 1},
+        {id: 9, name: 'kiwi', value: 1, 'categoryId': 1},
+        {id: 10, name: 'strawberry', value: 1, 'categoryId': 1},
+        {id: 11, name: 'orange', value: 1, 'categoryId': 1},
+        {id: 12, name: 'mango', value: 1, 'categoryId': 1},
+        {id: 13, name: 'steak', value: 1, 'categoryId': 4},
+        {id: 14, name: 'pork', value: 1, 'categoryId': 4},
+        {id: 15, name: 'turkey', value: 1, 'categoryId': 4},
+        {id: 16, name: 'salami', value: 1, 'categoryId': 4},
+        {id: 17, name: 'fish', value: 1, 'categoryId': 4},
+        {id: 18, name: 'carrot', value: 1, 'categoryId': 2},
+        {id: 19, name: 'cabbage', value: 1, 'categoryId': 2},
+        {id: 20, name: 'celery', value: 1, 'categoryId': 2},
+        {id: 21, name: 'tomato', value: 1, 'categoryId': 2},
+        {id: 22, name: 'potato', value: 1, 'categoryId': 2},
+        {id: 23, name: 'milk', value: 1, 'categoryId': 5},
+        {id: 24, name: 'butter', value: 1, 'categoryId': 5},
+        {id: 25, name: 'sour cream', value: 1, 'categoryId': 5},
+        {id: 26, name: 'buttermilk', value: 1, 'categoryId': 5},
+        {id: 27, name: 'yogurt', value: 1, 'categoryId': 5},
+
+
       ]);
     });
 };
